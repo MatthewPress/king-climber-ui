7645a834091fcf357e072dbe3a10027d4cb587f6
MatthewPress <press.matt14@gmail.com>
2022-11-09 14:40:48 -0700

    Feat: establish initial hooks to use data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 
 import Header from '../Header/Header';
@@ -11,6 +12,14 @@ import competitionData from '../../testData/competition-data';
 import './App.css';
 
 function App() {
+  const [climbers, setClimbers] = useState([]);
+  const [competitions, setCompetitions] = useState([]);
+
+  useEffect(() => {
+    setClimbers(climberData);
+    setCompetitions(competitionData);
+  }, []);
+
   return (
     <div className="app--container">
       <Header />
