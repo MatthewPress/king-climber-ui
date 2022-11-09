3e7f94346ef330a921b3b1902a9ef1ddddd0fdb8
MatthewPress <press.matt14@gmail.com>
2022-11-09 14:02:50 -0700

    Feat: add initial  area for past kings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 
 import Header from '../Header/Header';
 import CurrentKing from '../CurrentKing/CurrentKing';
+import PastKings from '../PastKings/PastKings';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="app--container">
       <Header />
       <CurrentKing />
+      <PastKings />
     </div>
   );
 }
---
+++ src/components/PastKings/PastKings.js
@@ -0,0 +1,9 @@
+import './PastKings.css';
+
+const PastKings = () => {
+  return (
+    <p>Past Kings</p>
+  );
+}
+
+export default PastKings;
