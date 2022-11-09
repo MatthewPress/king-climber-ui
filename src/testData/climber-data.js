e93bbd203cfb8033e1c48e30f7ab3701a2ad8b1e
MatthewPress <press.matt14@gmail.com>
2022-11-09 14:28:55 -0700

    Build: create test data

---
+++ src/testData/climber-data.js
@@ -0,0 +1,22 @@
+const climberData = {
+  climbers: [
+    {
+      "id": 1,
+      "name": "Matthew Press",
+      "img": ""
+    },
+    {
+      "id": 2,
+      "name": "Sed Tummer",
+      "img": ""
+    },
+    {
+      "id": 3,
+      "name": "Bogan Lishop",
+      "img": ""
+    }
+  ]
+};
+
+
+export default climberData;
