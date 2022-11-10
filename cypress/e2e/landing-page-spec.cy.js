1a86e0abaebe88aaaea3dc07718fab0eaff71804
MatthewPress <press.matt14@gmail.com>
2022-11-10 00:03:55 -0700

    Test: add initial tests for landing page

--- cypress/e2e/landing-page-spec.cy.js
+++ cypress/e2e/landing-page-spec.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Landing Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  });
+
+  it('Should display the header', () => {
+    cy.get('header').should('contain', 'King Climber');
+  });
+
+  it('Should display information about the current king climber', () => {
+    cy.get('main')
+      .get('.current-king--container')
+      .get('h2').should('contain', 'Sed Tummer')
+  });
+
+  it('Should', () => {
+    
+  });
+
 })
