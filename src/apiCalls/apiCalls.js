809979d37e31153a565bf9a7782071e8e5a0d421
MatthewPress <press.matt14@gmail.com>
2022-11-10 00:44:01 -0700

    Revert: remove test data function from api calls

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,20 +1,7 @@
-// Remove: when pulling data from API
-import climberData from '../testData/climber-data';
-import competitionData from '../testData/competition-data';
-
-const returnTestData = (path) => {
-  if (path === '/climbers') {
-    return climberData;
-  } else if (path === '/competitions') {
-    return competitionData;
-  }
-}
-
 const getData = async (path) => {
   try {
     let response = await fetch(`https://king-climber-api.replitted.repl.co${path}`);
     let data = await response.json();
-    // let data = response;
     return data;
   } catch (error) {
     console.log('Dang it');
