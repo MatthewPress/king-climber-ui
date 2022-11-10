7c689825d28cb76a9cefb69af45fc4d2159b5412
MatthewPress <press.matt14@gmail.com>
2022-11-09 22:39:48 -0700

    Feat: add king climber information

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,25 +12,35 @@ import './App.css';
 function App() {
   const [climbers, setClimbers] = useState([]);
   const [competitions, setCompetitions] = useState([]);
+  const [isClimbersLoading, setClimbersIsLoading] = useState(true);
+  const [isCompetitionLoading, setCompetitionIsLoading] = useState(true);
 
   useEffect(() => {
     getData(`/climbers`)
       .then(data => {
         setClimbers(data);
+        setClimbersIsLoading(false);
       });
 
     getData(`/competitions`)
       .then(data => {
         const sortedCompetitions = data.sort((a, b) => a.date < b.date ? 1 : -1);
         setCompetitions(sortedCompetitions);
+        setCompetitionIsLoading(false);
       });
   }, []);
 
   return (
     <div className="app--container">
       <Header />
-      <CurrentKing climbers={climbers} lastCompetition={competitions[0]} />
-      <PastKings />
+      {
+        isClimbersLoading || isCompetitionLoading
+          ? <p>Loading</p>
+          : <main>
+              <CurrentKing climbers={climbers} lastCompetition={competitions[0]} />
+              <PastKings />
+            </main> 
+      }
     </div>
   );
 }
--- src/components/CurrentKing/CurrentKing.js
+++ src/components/CurrentKing/CurrentKing.js
@@ -4,7 +4,13 @@ function CurrentKing({ climbers, lastCompetition }) {
   const kingClimber = climbers.find(climber => climber.id === lastCompetition.climber_id)
 
   return (
-    <p>{kingClimber.name}</p>
+    <div className="current-king--container">
+      <img src={kingClimber.pic} alt="Current King Climber Photo" />
+      <h2>{kingClimber.name}</h2>
+      <p>{lastCompetition.date}</p>
+      <p>{lastCompetition.location}</p>
+      <p>{lastCompetition.climb_grade}</p>
+    </div>
   );
 }
 
