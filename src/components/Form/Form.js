1fbf85ad83bfc65b85f2352edb572c180ad54ff3
MatthewPress <press.matt14@gmail.com>
2022-11-11 10:19:41 -0500

    Chore: add tentative data for form selects

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -17,7 +17,9 @@ function Form() {
         <label htmlFor="climber_id">
           Climber
           <select>
-            <option value=""></option>
+            <option value="Ted">Ted</option>
+            <option value="Matt">Matt</option>
+            <option value="Logan">Logan</option>
           </select>
         </label>
         <label htmlFor="date">
@@ -29,27 +31,31 @@ function Form() {
         </label>
         <label htmlFor="location">
           Location
-          <input
-            type="text"
-            name="location"
-          />
-        </label>
-        <label htmlFor="location">
-          Location
           <select>
-            <option value=""></option>
+            <option value="Movement Baker">Movement Baker</option>
+            <option value="Movement RiNo">Movement RiNo</option>
+            <option value="Movement Englewood">Movement Englewood</option>
           </select>
         </label>
         <label htmlFor="climb_type">
           Climb Type
           <select>
-            <option value=""></option>
+            <option value="Boulder">Boulder</option>
+            <option value="Top Rope">Top Rope</option>
+            <option value="Sport">Sport</option>
           </select>
         </label>
         <label htmlFor="climb_grade">
           Climb Grade
           <select>
-            <option value=""></option>
+            <option value="V1 / Green">V1 / Green</option>
+            <option value="V2 / Green">V2 / Green</option>
+            <option value="V3 / Yellow">V3 / Yellow</option>
+            <option value="V4 / Yellow">V4 / Yellow</option>
+            <option value="V5 / Orange">V5 / Orange</option>
+            <option value="V6 / Pink">V6 / Pink</option>
+            <option value="V7 / Pink">V7 / Pink</option>
+            <option value="5.11a">5.11a</option>
           </select>
         </label>
       <button type="submit">ADD COMP</button>
